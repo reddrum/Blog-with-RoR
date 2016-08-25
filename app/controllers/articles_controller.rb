d506fdf19dc3993ea63571ec485baabe153e7f0e
“reddrum” <anton.kranz@me.com>
2016-08-25 16:51:01 +0300

    Implement articles#show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,22 +1,26 @@
 class ArticlesController < ApplicationController
 
- def new
- end
-
- def create
-  @article = Article.new(article_params)
-  if @article.valid?
-   @article.save
-   redirect_to @article
-  else
-   render action: 'new'
+  def show
+   @article = Article.find(params[:id])
   end
- end
 
- private
+  def new
+  end
 
- def article_params
-  params.require(:article).permit(:title, :text)
- end
+  def create
+   @article = Article.new(article_params)
+   if @article.valid?
+    @article.save
+    redirect_to @article
+   else
+    render action: 'new'
+   end
+  end
+
+  private
+
+  def article_params
+   params.require(:article).permit(:title, :text)
+  end
 
 end
