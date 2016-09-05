16f3d852b6f9c1751e11575403831426594d7fcb
“reddrum” <anton.kranz@me.com>
2016-09-05 23:54:00 +0300

    Delete paperclip/add bootstrap

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,8 +3,6 @@ class Article < ActiveRecord::Base
   validates :text, presence: true
   has_many :comments
 
-  has_attached_file :image, styles: { large: "600x600>", medium: "300x300>" thumb: "150x150#" }
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   def subject
     title
